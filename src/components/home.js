6392f4464271d3d2b2c33d14c854f5582a9c079b
nattatorn <nattatorn@gmail.com>
2019-03-19 17:52:53 +0700

    home page production V.0
    redirec

--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react'
 import { Button } from 'reactstrap'
 import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 
 export default class home extends Component {
+
+  state = {
+    redirect: false
+  }
+
   writeDataToDevice = () => {
+    this.setState({ redirect: true })
+
     const command = {
       text: 'a04'
     }
     axios.post('/api/write', command)
-    console.log('ao4')
+
   }
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to='/thai-id' />
+    }
+  }
+
   render() {
     return (
       <div>
+        {this.renderRedirect()}
         <Button onClick={this.writeDataToDevice}>A04 (Thai ID)</Button>
       </div>
     )
