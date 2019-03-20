9e3355a598ceca9370f1c49138f50c5fea737552
nattatorn <nattatorn@gmail.com>
2019-03-20 13:55:27 +0700

    ThaiID V.1

--- src/components/layouts/thai_ID/thai_id.js
+++ src/components/layouts/thai_ID/thai_id.js
@@ -10,141 +10,35 @@ export default class show_data_test extends Component {
     super(props)
 
     this.state = {
-      data: []
+      data: [],
+      img64: ''
     }
     this.getDataFromServer()
   }
 
   getDataFromServer = () => {
     axios.get('/api/getData').then(result => {
-      console.log('DATA:', result)
-      this.setState({ data: result.data })
+      console.log('DATA:', result.data)
+      this.setState({ img64: result.data.data, data: result.data.data2 })
     })
   }
 
   render() {
-    const { data } = this.state
+    const { data, img64 } = this.state
+    const rawImg = "data:image/png;base64,"+img64
+    const rawData = Buffer.from(data, 'base64').toString()
     return (
       <div>
         <h1 className="title">Thai ID</h1>
         <Row>
           <Col xs="6">
             <img
-              src="https://static.naewna.com/uploads/news/source/332193.jpg"
+              src={rawImg}
               alt="img"
             />
           </Col>
           <Col xs="6">
-            {data.length > 0 ? (
-              data.map(result => (
-                <Form key={result.type}>
-                  {/* -----------ID Card---------- */}
-                  <FormGroup>
-                    <Label for="Indetification">Indetification Number</Label>
-                    <Input
-                      type="text"
-                      name="address"
-                      id="Indetification"
-                      value={result.data.firstname}
-                      readOnly
-                    />
-                  </FormGroup>
-                  {/* -----------Name---------- */}
-                  <Row>
-                    <Col md={6}>
-                      <FormGroup>
-                        <Label for="firstNmarThai">ชื่อ (ภาษาไทย)</Label>
-                        <Input
-                          type="text"
-                          name="firstNmarThai"
-                          id="firstNmarThai"
-                          value={result.data.firstname}
-                          readOnly
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col md={6}>
-                      <FormGroup>
-                        <Label for="SernameThai">นามสกุล (ภาษาไทย)</Label>
-                        <Input
-                          type="text"
-                          name="SernameThai"
-                          id="SernameThai"
-                          value={result.data.firstname}
-                          readOnly
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col md={6}>
-                      <FormGroup>
-                        <Label for="firstNmarEng">Name (English)</Label>
-                        <Input
-                          type="text"
-                          name="firstNmarEng"
-                          id="firstNmarEng"
-                          value={result.data.firstname}
-                          readOnly
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col md={6}>
-                      <FormGroup>
-                        <Label for="SernameEng">Sername (English)</Label>
-                        <Input
-                          type="text"
-                          name="SernameEng"
-                          id="SernameEng"
-                          value={result.data.firstname}
-                          readOnly
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  {/* -----------Address---------- */}
-                  <FormGroup>
-                    <Label for="exampleAddress">ที่อยู่</Label>
-                    <Input
-                      type="text"
-                      name="address"
-                      id="exampleAddress"
-                      value={result.data.firstname}
-                      readOnly
-                    />
-                  </FormGroup>
-                  {/* -----------Birth---------- */}
-                  <Row form>
-                    <Col md={6}>
-                      <FormGroup>
-                        <Label for="Birth">Date of Birth</Label>
-                        <Input
-                          type="text"
-                          name="Birth"
-                          id="Birth"
-                          value={result.data.firstname}
-                          readOnly
-                        />
-                      </FormGroup>
-                    </Col>
-                    {/* -----------Expiry---------- */}
-                    <Col md={6}>
-                      <FormGroup>
-                        <Label for="Expiry">Date of Expiry</Label>
-                        <Input
-                          type="text"
-                          name="Expiry"
-                          id="Expiry"
-                          value={result.data.firstname}
-                          readOnly
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  {/* <Button>Sign in</Button> */}
-                </Form>
-              ))
-            ) : (
-              <Alert color="danger">Don't have any data</Alert>
-            )}
+            <p>{rawData}</p>
           </Col>
         </Row>
       </div>
