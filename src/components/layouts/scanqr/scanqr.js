7a8e172a3e01e33424146c525289286c9c0de7a3
DESKTOP-DI4G0T8\terLOOK <terlook52285@gmail.com>
2019-03-21 15:37:13 +0700

    scan qr V.0

--- src/components/layouts/scanqr/scanqr.js
+++ src/components/layouts/scanqr/scanqr.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import '../../../App.css'
 
 export default class scanqr extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      text: ''
+    }
+    setTimeout(this.getDataFromServer, 1000)
+  }
+
+  getDataFromServer = () => {
+    console.log('GET data from server')
+    axios.get('/api/getData').then(result => {
+      console.log('DATA:', result.data)
+      this.setState({ text: result.data.img64 })
+    })
+  }
+
   render() {
+    const { text } = this.state
     return (
       <div>
-         <h1 className="title">scan QR</h1>
+        <h1 className="title">scan QR</h1>
+        {text}
       </div>
     )
   }
